dc19cc51cefdcc6858a3b15116dcc3952a1630e1
Anastasiia7878 <dr.saymon78@gmail.com>
2023-05-17 00:02:05 +0300

    Summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,7 +7,7 @@ galleryItems.forEach(item =>
     `<li class="gallery__item">
         <a class="gallery__link" href="${item.original}">
             <img
-            class= "gallery_image"
+            class= "gallery__image"
             src="${item.preview}"
             data-source="${item.original}"
             alt="${item.description}"
